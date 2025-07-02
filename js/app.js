4d4a17e9cab87d5650e583ed71fadd3f73b1ec81
k7dbh <203570242+k7dbh@users.noreply.github.com>
2025-07-02 13:27:28 +0300

    recently added css and updated the html file

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 /*-------------- Constants -------------*/
-
-
+const word = ['cat','lion','dog','zebra','bird','hamster','goat','pig','horse','wolf','banana','mouse','banana','orange','table','board','chair','paper','card','pen','door','wire','computer']
+const misTimes = 6;//6 times maximum number of mistakes
 
 
 
 /*---------- Variables (state) ---------*/
 
-
-
+let loseGame = false;
+let guessWrong = 0;
+let guessLetter = [];
 
 
 /*----- Cached Element References  -----*/
-
-
+const spaceManEl = document.querySelector('.space-man');
+const wordEl = document.querySelector('.word');
+const keyBtnEl = document.querySelectorAll('.key');
+const msgEl = document.querySelector('.message');
+const resetBtn = document.querySelector('.reset');
+console.log(wordEl)
 
 
 /*-------------- Functions -------------*/
