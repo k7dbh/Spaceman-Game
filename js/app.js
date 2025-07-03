eed33cecb8fddfa8ba80841b3ca77883864ff078
k7dbh <203570242+k7dbh@users.noreply.github.com>
2025-07-03 10:53:31 +0300

    issue solved working on game instructions

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 /*-------------- Constants -------------*/
-const words = ['cat','lion','dog','zebra','bird','hamster','goat','pig','horse','wolf','banana','mouse','banana','orange','table','board','chair','paper','card','pen','door','wire','computer','pencil','table','teacher','window','flower','water','panda','candy','bag','glass','hat','pizza','burger','juice','tower',];
+const words = ['cat', 'lion', 'dog', 'zebra', 'bird', 'hamster', 'goat', 'pig', 'horse', 'wolf', 'banana', 'mouse', 'banana', 'orange', 'table', 'board', 'chair', 'paper', 'card', 'pen', 'door', 'wire', 'computer', 'pencil', 'table', 'teacher', 'window', 'flower', 'water', 'panda', 'candy', 'bag', 'glass', 'hat', 'pizza', 'burger', 'juice', 'tower',];
 const misTimes = 6; // maximum number of wrong guesses
-const spaceMnPc = ['🏃‍♂️‍➡️____________ ','__🏃‍♂️‍➡️__________','____🏃‍♂️‍➡️________','_______🏃‍♂️‍➡️_____','__________🏃‍♂️‍➡️__','___________🏃‍♂️‍➡️_','___________🪦😵'];
+const spacePics = ['🏃‍➡️__________________', '_____🏃‍➡️_____________', '________🏃‍➡️__________', '____________🏃‍➡️______', '______________🏃‍➡️____', '________________🏃‍➡️__', '______________🪦😵'];
 
 /*---------- Variables ---------*/
 let wordRdm = '';
 let guessLetter = []
 let guessWrong = 0;
-let loseGame = false; 
+let loseGame = false;
 
 /*----- Cached Element References  -----*/
 const spaceManEl = document.querySelector('.spaceman-art');
@@ -25,8 +25,8 @@ function startingGame() {
     guessWrong = 0;
     loseGame = false;
     msgEl.textContent = '';
-    
-    
+
+    keyBtnEl.forEach(btn => btn.disabled = false);
     renderUI();
 }
 
@@ -35,7 +35,7 @@ function guessWord(letter, btn) {
     if (loseGame || guessLetter.includes(letter)) return;
     guessLetter.push(letter);
     // disable the button after guess
-    
+    btn.disabled = true;
 
     // check if letter in the word
     if (!wordRdm.includes(letter.toLowerCase())) guessWrong++;
@@ -47,28 +47,29 @@ function guessWord(letter, btn) {
 
 // check for win or lose
 function winLose() {
-      if (wordRdm.split('').every(a => guessLetter.includes(a.toUpperCase()))) {
+    if (wordRdm.split('').every(a => guessLetter.includes(a.toUpperCase()))) {
         loseGame = true;
-        msgEl.textContent = 'You Win!';
+        msgEl.textContent = 'You Win!!!';
         keyBtnEl.forEach(btn => btn.disabled = true);
-      } else if (guessWrong >= misTimes) {
+    } else if (guessWrong >= misTimes) {
         loseGame = true;
-        msgEl.textContent = `You Lose! Word: "${wordRdm.toUpperCase()}"`;
+        msgEl.textContent = `You Lose! Word: "${wordRdm.toUpperCase()}"
+        Try again`;
         keyBtnEl.forEach(btn => btn.disabled = true);
-      }
+    }
 }
 // Render UI functions
 function renderSpaceMan() {
-    spaceManEl.textContent = spaceMnPc[guessWrong];
+    spaceManEl.textContent = spacePics[guessWrong];
 }
 
 
 function renderWord() {
-      wordEl.innerHTML = '';
-      for (let a of wordRdm) {
+    wordEl.innerHTML = '';
+    for (let a of wordRdm) {
         wordEl.innerHTML += guessLetter.includes(a.toUpperCase()) ? a.toUpperCase() + ' ' : '_ ';
-      }
     }
+}
 
 function renderMsg() {
     if (!loseGame) {
@@ -80,15 +81,16 @@ function renderMsg() {
 function renderUI() {
     renderSpaceMan();
     renderWord();
+    renderMsg();
 }
 
 /*----------- Event Listeners ----------*/
 keyBtnEl.forEach(btn => {
-    btn.addEventListener('click', function() {
+    btn.addEventListener('click', function () {
         guessWord(btn.textContent, btn);
     });
 });
 
- resetBtn.addEventListener('click', startingGame);
+resetBtn.addEventListener('click', startingGame);
 // Initialize game
 startingGame();
